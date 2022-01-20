30cb16681d8700650e623a4540528645736130ac
aljora94 <97757966+aljora94@users.noreply.github.com>
2022-01-20 19:53:24 +0100

    PrimeraVC
    PrimeraVC

---
+++ MiDataNavigation/PrimeraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PrimeraViewController.swift
+//  MiDataNavigation
+//
+//  Created by Alberto Yagüe on 20/1/22.
+//
+
+import UIKit
+
+class PrimeraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
