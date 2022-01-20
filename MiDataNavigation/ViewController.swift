060ff68f68e3464c717016706d29d80e2e580ae1
aljora94 <97757966+aljora94@users.noreply.github.com>
2022-01-20 19:49:53 +0100

    Initial commit

---
+++ MiDataNavigation/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiDataNavigation
+//
+//  Created by Alberto Yagüe on 20/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
